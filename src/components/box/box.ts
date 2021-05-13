f9a6a6751464c4f5d27c20c19b609cbaf92d11e9
BEFARnvkz <befar91@gmail.com>
2021-05-13 09:04:36 +0700

    2

--- src/components/box/box.ts
+++ src/components/box/box.ts
@@ -3,6 +3,7 @@ import Component, { ComponentProps } from '../../app/js/component';
 export default class Box extends Component.Default {
     constructor(element: ComponentProps) {
         super(element);
+        console.log(1);
     }
 
     destroy = () => {
